95c29a0b76cc845e5ba48778bfab27dd9022d88e
suyons <su02ga@outlook.com>
2025-06-24 00:23:46 +0900

    Feat: add bulk fetch candlesticks on rest client

--- src/service/gate_rest_client.py
+++ src/service/gate_rest_client.py
@@ -2,8 +2,9 @@ import requests
 
 
 class GateRestClient:
-    def __init__(self, url):
+    def __init__(self, url, ohlcv_count):
         self.url = url
+        self.ohlcv_count = ohlcv_count
         self.headers = {
             "Accept": "application/json",
             "Content-Type": "application/json",
@@ -35,6 +36,38 @@ class GateRestClient:
         path = f"/futures/usdt/candlesticks"
         response = requests.get(self.url + path, headers=self.headers, params=params)
         return response.json()
+
+    def get_futures_candlesticks_bulk(self, params):
+        """
+        If 'limit' is greater than 1000, repeatedly call the API to fetch the desired amount of ohlcv data.
+
+        Args:
+            params (dict): Same as get_futures_candlesticks.
+                - Enter the total desired count in the 'limit' key.
+
+        Returns:
+            list: Accumulated list of candlestick data
+        """
+        all_data = []
+        total_limit = self.ohlcv_count if self.ohlcv_count else 1000
+        fetch_limit = 1000
+        params["limit"] = min(fetch_limit, total_limit)
+
+        while len(all_data) < total_limit:
+            data = self.get_futures_candlesticks(params)
+            if not data:
+                break
+            all_data = data + all_data
+            if len(data) < fetch_limit:
+                break  # No more data available
+
+            # Update the 'from' parameter for the next request with the last candlestick's timestamp + 1
+            first_timestamp = float(data[0]["t"])
+            params["to"] = int(first_timestamp) - 60
+            remain = total_limit - len(all_data)
+            params["limit"] = min(fetch_limit, remain)
+
+        return all_data[:total_limit]
 
 
 # 사용 예시
